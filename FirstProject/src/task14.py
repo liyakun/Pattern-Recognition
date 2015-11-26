bcac129f80812f52ffc32b370da850cd5010043f
yakun <liyakun127@gmail.com>
2015-11-26 21:02:48 +0100

    clean comments

--- FirstProject/src/task14.py
+++ FirstProject/src/task14.py
@@ -1,11 +1,9 @@
-"""
-We need the p norm, basic idea:
-    1. draw all the possible point with x = 1, ||1|| with p = 1/2
-    2. when p = 0.5, the Minkoski violates the triangle
-"""
 from lib import PlotCircle
 
+
 class Task14:
+    """Implement the task 1.4
+    """
 
     def __init__(self):
         pass
