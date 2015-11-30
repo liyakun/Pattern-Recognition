15cb3744d126dcc4809b1b958a5d624ebe1e7aca
yobibyte <vitaliykurin@gmail.com>
2015-11-30 01:03:15 +0100

    tast13 in progress

--- FirstProject/src/task13.py
+++ FirstProject/src/task13.py
@@ -20,6 +20,7 @@ class Task13:
 
         # read queries information into 1D array
         raw = [d[1] for d in data if d[1] != 0]
+        x_raw = [i+1 for i in range(len(raw))]
 
         #hist->independent observations
         x  = [k for el in xrange(1,len(raw)+1) for k in [el]*el]
@@ -27,36 +28,26 @@ class Task13:
 
         N = len(x)
         epochs = range(10)
-        al = pow(np.mean(x),2)/np.var(x);
-        la=np.mean(x)/np.var(x)
-        arr = np.mat([al,la]).T
-        dldk_p = 0
+        arr = np.mat([1,1]).T
         for e in epochs:
             k = arr[0,0]
             a = arr[1,0]
-            dldk = N/k - N*log(a)
-            dlda = -N
-            d2ldk2 = -N/(k*k)
-            d2lda2 = 0
-            d2ldkda = -N/a
-
-            for d in hs:
-                datok = pow((d/a),k)
-                logda = log(d/a)
-                dldk += (log(d) - datok*logda)
-                dlda   += datok
-                d2ldk2 -= datok*logda*logda
-                d2lda2 += datok
-                d2ldkda = d2ldkda + 1/a * datok + k/a * datok*logda
-            dlda*=(k/a)
-            d2lda2 = k*(N - (k+1)*d2lda2)/(a*a)
+            sdatok = sum([pow(d/a,k) for d in hs])
+            sldatok = sum([pow(d/a,k)*log(d/a) for d in hs])
+            dldk = N/k - N*log(a) + sum([log(d) for d in hs]) - sldatok
+            dlda = (k/a)*(sdatok-N)
+            d2ldk2 = -N/(k*k) - sum([pow(d/a,k)*pow(log(d/a),2) for d in hs])
+            d2lda2 = (k/(a*a))*(N-(k+1)*sdatok)
+            d2ldkda = -N*a+(1/a)*sdatok + (k/a)*sldatok
             hessian = np.mat([[d2ldk2,d2ldkda],[d2ldkda,d2lda2]])
             gr = np.mat([-dldk, -dlda]).T
             arr = arr + linalg.inv(hessian)*gr
 
-        distribution = [k/a * pow(el/a,k-1) * (exp(-pow(el/a,k))) for el in hs]
+
+        distribution = [k/a * pow(el/a, k-1) * (exp(-pow(el/a, k))) for el in x_raw]
         # plot and write to disk
-        x_matrix = np.vstack((x, distribution))
+        x_matrix = np.vstack((x_raw, distribution))
         Plot2dData().plot_data_2d(x_matrix, '../results/task13/plotQueries.pdf')
+
 
 
