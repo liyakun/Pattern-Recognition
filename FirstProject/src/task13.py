19841e49d5a1df0d86a4df188b693b1ffca602b2
liyakun <liyakun127@gmail.com>
2015-12-04 19:58:17 +0100

    create 'read_and_plot_opt' function in task13.py without creating large set of numbers d_i

--- FirstProject/src/task13.py
+++ FirstProject/src/task13.py
@@ -11,6 +11,50 @@ class Task13:
     def __init__(self):
         pass
 
+    def read_and_plot_opt(self):
+        """
+        read weight, height and gender information from file, and plot weight against height
+        """
+        dt = np.dtype([('date', np.str_, 24), ('queries', np.int)])
+        data = np.loadtxt('../resources/myspace.csv', dtype=dt, comments='#', delimiter=',')
+
+        # read queries information into 1D array
+        raw = [d[1] for d in data if d[1] != 0]
+        x_raw = [i+1 for i in range(len(raw))]
+
+        N = sum(raw)
+
+        epochs = range(20)
+        arr = np.mat([1.0, 1.0]).T
+        for e in epochs:
+            k = arr[0, 0]
+            a = arr[1, 0]
+            print k, a
+
+            sldatok = sum([h * np.power((x / a), k) * log(x / a) for h, x in zip(raw, x_raw)])
+            sdatok = sum([h * np.power((x / a), k) for h, x in zip(raw, x_raw)])
+            sd2ldk2 = sum([h * np.power((x / a), k) * pow(log(x / a), 2) for h, x in zip(raw, x_raw)])
+            slogd = sum([h * log(x) for h, x in zip(raw, x_raw)])
+
+            dldk = N/k - N*log(a) + slogd - sldatok
+            dlda = (k/a)*(sdatok-N)
+            d2ldk2 = -N/(k*k) - sd2ldk2
+            d2lda2 = (k/(a*a))*(N-(k+1)*sdatok)
+            d2ldkda = (1/a)*sdatok + (k/a)*sldatok - N/a
+
+            hessian = np.mat([[d2ldk2, d2ldkda], [d2ldkda, d2lda2]])
+            gr = np.mat([-dldk, -dlda]).T
+            arr = arr + np.dot(linalg.inv(hessian), gr)
+
+        #distribution scaled by N
+        distribution = [(N*(k/a * pow(el/a, k-1) * (exp(-pow(el/a, k))))) for el in x_raw]
+
+        # plot and write to disk
+        x_matrix = np.vstack((x_raw, raw))
+        Plot2dData().plot_data_2d(x_matrix, '3', distribution, '../results/task13/plotQueries.pdf')
+
+
+
     def read_and_plot(self):
         """
         read weight, height and gender information from file, and plot weight against height
@@ -31,9 +75,12 @@ class Task13:
         for e in epochs:
             k = arr[0, 0]
             a = arr[1, 0]
+            print sum([log(d) for d in hs])
 
             sdatok = sum([pow(d/a, k) for d in hs])
             sldatok = sum([pow(d/a, k)*log(d/a) for d in hs])
+
+            print sdatok, sldatok
 
             dldk = N/k - N*log(a) + sum([log(d) for d in hs]) - sldatok
             dlda = (k/a)*(sdatok-N)
